11aa06f7d30bd9f7bd46385e803495b25186c1a7
JIBSON082 <105949776+JIBSON082@users.noreply.github.com>
2026-05-26 08:02:46 +0100

    Update FoodCarousel.tsx

--- src/components/FoodCarousel.tsx
+++ src/components/FoodCarousel.tsx
@@ -135,7 +135,7 @@ export default function FoodCarousel() {
           maxWidth: "520px",
           margin: "1.5rem auto 0",
           aspectRatio: "1 / 0.8",
-          top: "-6px",
+          top: "-5px",
         }}
       >
         {/* Dashed orbit ellipse */}
