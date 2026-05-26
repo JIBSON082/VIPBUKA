bdcbf956beb1b08938d7d393ad3c9e8fd1f5c740
JIBSON082 <105949776+JIBSON082@users.noreply.github.com>
2026-05-26 07:22:13 +0100

    Update FoodCarousel.tsx

--- src/components/FoodCarousel.tsx
+++ src/components/FoodCarousel.tsx
@@ -133,7 +133,7 @@ export default function FoodCarousel() {
           position: "relative",
           width: "100%",
           maxWidth: "520px",
-          margin: "-2rem auto 0",
+          margin: "0.5rem auto 0",
           aspectRatio: "1 / 0.9",
         }}
       >
